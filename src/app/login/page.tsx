07ef89019408d6c5e41b7f58b46a513862399afc
JJIIIINN <kgb21537@gmail.com>
2023-09-29 02:09:52 +0900

    로그인 페이지 퍼블리싱

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Login from "@/components/element/login";
+import React from "react";
+import styled from "styled-components";
+
+export default function LoginPage() {
+    return (
+        <>
+            <Login />
+        </>
+    );
+}
