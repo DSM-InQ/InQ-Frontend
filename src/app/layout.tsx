16bf6d19e603f2c1100c06c760b228166b73e917
JJIIIINN <kgb21537@gmail.com>
2023-09-30 01:02:50 +0900

    setting: 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-"use client";
 import "@/styles/globals.css";
 import { Noto_Sans_KR } from "next/font/google";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Metadata } from "next";
+import Provider from "@/components/Provider";
 
 const notoSans = Noto_Sans_KR({
     weight: ["400", "500", "700"],
@@ -11,11 +10,11 @@ const notoSans = Noto_Sans_KR({
     display: "swap",
 });
 
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: { retry: 0 },
-    },
-});
+export const metadata: Metadata = {
+    title: "InQ",
+    description:
+        "대덕소프트웨어마이스터고등학교 2학년 학생들이 면접질문을 연습할 수 있도록 만든 서비스입니다.",
+};
 
 export default function RootLayout({
     children,
@@ -24,19 +23,8 @@ export default function RootLayout({
 }) {
     return (
         <html lang="ko">
-            <head>
-                <title>InQ</title>
-                <link rel="icon" href="./favicon.ico" />
-                <meta
-                    name="description"
-                    content="대덕소프트웨어마이스터고등학교 2학년 학생들이 면접질문을 연습할 수 있도록 만든 서비스입니다."
-                />
-            </head>
             <body className={notoSans.className}>
-                <QueryClientProvider client={queryClient}>
-                    {children}
-                    <ReactQueryDevtools initialIsOpen={true} />
-                </QueryClientProvider>
+                <Provider>{children}</Provider>
             </body>
         </html>
     );
