4d53758c9730f1f40491ed70332ea5c88c5a06d9
서유정 <seoyujeong1234@gmail.com>
2023-11-10 23:45:35 +0900

    feat: 내 정보 페이지 type 파일 추가

---
+++ src/apis/user/type.ts
@@ -0,0 +1,17 @@
+export interface myInfoResponse {
+    username: string;
+    join_date: string;
+    coin: number;
+    job: string;
+    job_duration: number;
+}
+
+export interface checkResponse {
+    monday: boolean;
+    tuesday: boolean;
+    wednesday: boolean;
+    thursday: boolean;
+    friday: boolean;
+    saturday: boolean;
+    sunday: boolean;
+}
